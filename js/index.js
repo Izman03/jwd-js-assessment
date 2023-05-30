36ced53b2501d7e94f0241fead640b4e4d64292c
Izmael Holmes <izmael.holmes165@gmail.com>
2023-05-30 10:50:45 +0930

    JS

--- js/index.js
+++ js/index.js
@@ -1,30 +1,11 @@
-/* ***************************
-  JWD JavaScript Assessment
-
-  This code is unfinished. You will need to study it to figure out what it does. Then you will need to use this and
-  your own code, to finish the app. 
-  
-  The tasks you need to do are below.
-
-    TASKS TODO:
-      1. Calculate the score as the total of the number of correct answers
-
-      2. Add an Event listener for the submit button, which will display the score and highlight 
-         the correct answers when the button is clicked. Use the code from lines 67 to 86 to help you.
-
-      3. Add 2 more questions to the app (each question must have 4 options).
-
-      4. Reload the page when the reset button is clicked (hint: search window.location)
-
-      5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
-*************************** */
-
 window.addEventListener('DOMContentLoaded', () => {
   const start = document.querySelector('#start');
   start.addEventListener('click', function (e) {
     document.querySelector('#quizBlock').style.display = 'block';
     start.style.display = 'none';
+    startQuizTimer();
   });
+
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
@@ -40,17 +21,98 @@ window.addEventListener('DOMContentLoaded', () => {
       a: 3,
     },
     {
-      q: 'What is the capital of Australia',
+      q: 'What is the capital of Australia?',
       o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
       a: 1,
     },
   ];
 
-  // function to Display the quiz questions and answers from the object
+  let timer; // Timer variable
+
+  // Function to start the quiz timer
+  const startQuizTimer = () => {
+    const countDownTime = 60; // Countdown time in seconds
+    let timeRemaining = countDownTime;
+    updateTimerDisplay(timeRemaining);
+
+    timer = setInterval(() => {
+      timeRemaining--;
+      updateTimerDisplay(timeRemaining);
+
+      if (timeRemaining <= 0) {
+        endQuiz();
+      }
+    }, 1000);
+  };
+
+  // Function to update the timer display
+  const updateTimerDisplay = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    const timerDisplay = document.querySelector('#time');
+    timerDisplay.textContent = `${padZero(minutes)}:${padZero(seconds)}`;
+  };
+
+  // Function to pad zero to single-digit numbers
+  const padZero = (num) => {
+    return num < 10 ? '0' + num : num;
+  };
+
+  // Function to end the quiz and display the score
+  const endQuiz = () => {
+    clearInterval(timer); // Stop the timer
+    calculateScore(); // Calculate the score
+    showScore(); // Display the score
+  };
+
+  // Function to calculate the score
+  const calculateScore = () => {
+    let score = 0;
+    quizArray.forEach((quizItem, index) => {
+      for (let i = 0; i < 4; i++) {
+        let r = `radio_${index}_${i}`;
+        let radioElement = document.querySelector('#' + r);
+
+        if (quizItem.a === i && radioElement.checked) {
+          score++; // Increment the score if the correct answer is selected
+        }
+      }
+    });
+
+    return score;
+  };
+
+  // Function to display the score
+  const showScore = () => {
+    const score = calculateScore();
+    const scoreDisplay = document.querySelector('#score');
+    scoreDisplay.textContent = `Your score: ${score} out of ${quizArray.length}`;
+    highlightCorrectAnswers();
+  };
+
+  // Function to highlight the correct answers
+  const highlightCorrectAnswers = () => {
+    quizArray.forEach((quizItem, index) => {
+      for (let i = 0; i < 4; i++) {
+        let li = `li_${index}_${i}`;
+        let r = `radio_${index}_${i}`;
+        let liElement = document.querySelector('#' + li);
+        let radioElement = document.querySelector('#' + r);
+
+        if (quizItem.a === i) {
+          liElement.style.backgroundColor = 'lightgreen'; // Highlight the correct answer
+        }
+
+        radioElement.disabled = true; // Disable the radio buttons
+      }
+    });
+  };
+
+  // Function to display the quiz questions and answers from the object
   const displayQuiz = () => {
     const quizWrap = document.querySelector('#quizWrap');
     let quizDisplay = '';
-    quizArray.map((quizItem, index) => {
+    quizArray.forEach((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
                    Q - ${quizItem.q}
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
@@ -63,28 +125,27 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  // Calculate the score
-  const calculateScore = () => {
-    let score = 0;
-    quizArray.map((quizItem, index) => {
-      for (let i = 0; i < 4; i++) {
-        //highlight the li if it is the correct answer
-        let li = `li_${index}_${i}`;
-        let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
-
-        if (quizItem.a == i) {
-          //change background color of li element here
-        }
-
-        if (radioElement.checked) {
-          // code for task 1 goes here
-        }
-      }
-    });
+  // Function to reset the quiz
+  const resetQuiz = () => {
+    clearInterval(timer); // Stop the timer
+    document.querySelector('#quizBlock').style.display = 'none';
+    document.querySelector('#start').style.display = 'block';
+    document.querySelector('#score').textContent = '';
+    displayQuiz(); // Re-display the quiz questions
   };
 
-  // call the displayQuiz function
+  // Event listener for the submit button
+  const submitBtn = document.querySelector('#btnSubmit');
+  submitBtn.addEventListener('click', () => {
+    endQuiz();
+  });
+
+  // Event listener for the reset button
+  const resetBtn = document.querySelector('#btnReset');
+  resetBtn.addEventListener('click', () => {
+    resetQuiz();
+  });
+
+  // Call the displayQuiz function
   displayQuiz();
 });
